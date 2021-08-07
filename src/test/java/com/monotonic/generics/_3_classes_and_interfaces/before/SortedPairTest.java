9ec9571881fe407b9261e88805784afe0b6b1a44
Bora Guven <guvenbe@gmail.com>
2021-08-06 21:57:43 -0400

    Initial commit

---
+++ src/test/java/com/monotonic/generics/_3_classes_and_interfaces/before/SortedPairTest.java
@@ -0,0 +1,28 @@
+package com.monotonic.generics._3_classes_and_interfaces.before;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortedPairTest
+{
+    @Test
+    public void shouldRetainOrderOfOrderedPair()
+    {
+        SortedPair<Integer> pair = new SortedPair<>(1, 2);
+
+        assertEquals(1, pair.getFirst().intValue());
+        assertEquals(2, pair.getSecond().intValue());
+    }
+
+    @Test
+    public void shouldFlipOrderOfMisorderedPair()
+    {
+        SortedPair<Integer> pair = new SortedPair<>(2, 1);
+
+        assertEquals(1, pair.getFirst().intValue());
+        assertEquals(2, pair.getSecond().intValue());
+    }
+
+}
